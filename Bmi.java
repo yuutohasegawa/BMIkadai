7681d5e753b417e1974c0c88321d4b1861fa4234
長谷川勇斗 <y.hasegawa.sys23@morijyobi.ac.jp>
2023-11-10 09:45:44 +0900

    BMI評価を追加

--- Bmi.java
+++ Bmi.java
@@ -1,22 +1,22 @@
- import java.util.Scanner;
+import java.util.Scanner;
 
-public class Bmi {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("1人目の情報を入力してください:");
         System.out.print("身長を入力してください※小数点は切り捨てで入力してください: ");
-        int height1 = scanner.nextInt();
+        int height1 = scanner.nextInt(); // 1人目の身長を整数として受け取る
 
         System.out.print("体重を入力してください※小数点は切り捨てで入力してください: ");
-        int weight1 = scanner.nextInt();
+        int weight1 = scanner.nextInt(); // 1人目の体重を整数として受け取る
 
         System.out.println("2人目の情報を入力してください:");
         System.out.print("身長を入力してください※小数点は切り捨てで入力してください: ");
-        int height2 = scanner.nextInt();
+        int height2 = scanner.nextInt(); // 2人目の身長を整数として受け取る
 
         System.out.print("体重を入力してください※小数点は切り捨てで入力してください: ");
-        int weight2 = scanner.nextInt();
+        int weight2 = scanner.nextInt(); // 2人目の体重を整数として受け取る
 
         double bmi1 = calculateBMI(weight1, height1);
         double bmi2 = calculateBMI(weight2, height2);
@@ -43,18 +43,18 @@ public class Bmi {
 
     public static String interpretBMI(double bmi) {
         if (bmi < 18.5) {
-            return "低体重";
+            return "<低体重> 貧血を引き起こし、疲れやすい、だるいなどの症状があらわれます。";
         } else if (bmi < 24.9) {
-            return "正常体重";
+            return "<正常体重>　nice!";
         } else if (bmi < 29.9) {
-            return "肥満度1度";
+            return "肥満度1度  2型糖尿病や循環器疾患のリスクが高くなり、内臓脂肪が増えていきます。";
         } else if (bmi < 34.9) {
-            return "肥満度2度";
+            return "<肥満度2度> 死亡リスクが94%になります。";
         } else if (bmi < 39.9) {
-            return "肥満度3度";
+            return "<肥満度3度> 死亡リスクが2度に比べて3倍になりますになります。";
         } else {
-            return "肥満度4度";
+            return "<肥満度4度> 頑張りましょう。";
         }
     }
 }
- 
+
